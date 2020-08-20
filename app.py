50eb084d7deb7598bace57130b9f17aaeb327096
harshmalviya7 <52200329+harshmalviya7@users.noreply.github.com>
2020-08-21 05:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('tranform.pkl','rb'))
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
-@cross_origin()
+
 def home():
 	return render_template('index.html')
 @app.route('/predict',methods=['POST'])
